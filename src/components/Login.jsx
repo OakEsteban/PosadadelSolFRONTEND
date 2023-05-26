2dcc1841d33013dd323ee818f052ccaf01687ef8
Cris25nkl <cristian25florez@gmail.com>
2023-05-26 18:39:44 -0500

    Update: Cosas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,53 @@
-import img1 from '../Images/hotel.png';
-import logoPosada from '../Images/logoPosada.png';
-import '../Styles/Login.css';
-import React, {useState} from 'react';
+import img1 from "../Images/hotel.png";
+import logoPosada from "../Images/logoPosada.png";
+import "../Styles/Login.css";
+import React, { useState } from "react";
 //import {useform} from "react-hook-form"
 
-function login(){
-    return (
+function login() {
+  return (
     <div className="App">
-    <header className="App-header">
-    <div>
+      <header className="App-header">
+        <div>
+          <div class="titulo">
+            <img src={logoPosada} alt="logo" width="100" height="100"></img>
+            <a type="nombre" href="http//">
+              La posada del sol
+            </a>
+          </div>
 
-      <div class = "titulo">
-        <img src={logoPosada} alt="logo" width="100" height="100"></img>
-        <a type="nombre">La posada del sol</a>
-      </div>
+          <h2>Ingresar</h2>
+          <form>
+            <div class="input-group">
+              <label>Usuario</label>
+              <input type="text" class="textField" name=""></input>
+            </div>
+            <div class="input-group">
+              <label>Constraseña</label>
+              <input type="password" class="textField" name=""></input>
+            </div>
 
-      <h2>Ingresar</h2>
-      <form>
+            <div class="buttons">
+              <input type="checkbox" id="check"/>
+              <label htmlFor="check"><b> Recordarme </b></label>
+              <a href="http//" className="password">
+                {" "}
+                <b> Olvidé mi Constraseña </b>
+              </a>
+              <input type="submit" value="Ingresar"></input>
+              <p className="registro">
+                <b> ¿No tienes una cuenta?</b> <a href="#"> Registrarse </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </header>
 
-              <div class = "input-group">
-                <label>Usuario</label>
-                <input type="text" class="textField" name=""></input>
-              </div>
-              <div class = "input-group">
-                  <label>Constraseña</label>
-                  <input type="password" class="textField" name=""></input>
-              </div>
-
-              <div class = "buttons">
-              <a href='#' className='password'>Olvidé mi Constraseña</a>
-              <input type='submit' value="Ingresar"></input>
-              <p className='registro'>¿No tienes una cuenta?  <a href='#'> Registrarse </a></p>
-              </div>
-  
-        </form>
-      
-  </div>
-    </header>
-
-   <body className="App-body">
-      <img class = 'images' src={img1} alt="logo" width="500" ></img>
-    </body> 
-  </div>
-    )
-
+      <body className="App-body">
+        <img class="images" src={img1} alt="logo" width="500"></img>
+      </body>
+    </div>
+  );
 }
 
 export default login;
