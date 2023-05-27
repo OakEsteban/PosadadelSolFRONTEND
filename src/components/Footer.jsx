216a28cfe70d76828e9571f5de0f240e0836667f
oakEsteban <joann.es.jebl@gmail.com>
2023-05-27 17:11:41 -0500

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,76 @@
+import logoPosada from "../Images/logoPosada.png";
+import "../Styles/Footer.css";
+import React, { useState } from "react";
+//import {useform} from "react-hook-form"
+
+function Footer() {
+  return (
+    <footer>
+        <div className = "footerTop">
+            <div className="about">
+                <h1>Acerca de nosotros</h1>
+                <a className = "descriptionText" >Nuestro hotel ofrece...</a>
+            </div>
+            <div className="subscription">
+                <h3>¡Suscribete para recibir ofertas especiales!</h3>
+                <div className = "ofertaContainer">
+                    <input className = "inputEmail" placeholder="Tu dirección de email"></input>
+                    <button className = "button1" type="submit">Suscribirse</button>
+                </div>   
+            </div>
+        </div>
+
+        <div className="footerBottom">
+            <div className="posada box">
+                <img src={logoPosada} className="logo" width="100" height="100"></img>
+                <a className="posadaText">La posada del sol</a>
+            </div>
+            <div className="box">
+                <div className="list">
+                    <h3>Nuestro hotel</h3>
+                    <a href="#">Habitacion1</a>
+                    <a href="#">Habitacion2</a>
+                    <a href="#">Habitacion3</a>
+                </div>
+            </div>
+            <div className="box">
+                <div className="list">
+                    <h3>Nuestras habitaciones</h3>
+                    <a href="#">Habitacion1</a>
+                    <a href="#">Habitacion2</a>
+                    <a href="#">Habitacion3</a>
+                </div>
+            </div>
+            <div className="box">
+                <div className="list">
+                    <h3>Nuestros servicios</h3>
+                    <a href="#">Habitacion1</a>
+                    <a href="#">Habitacion2</a>
+                    <a href="#">Habitacion3</a>
+                </div>
+            </div>
+            <div className="box">
+                <div className="list">
+                    <h3>Acerca de nosotros</h3>
+                    <a href="#">Habitacion1</a>
+                    <a href="#">Habitacion2</a>
+                    <a href="#">Habitacion3</a>
+                </div>
+            </div>
+            <div className="box">
+                <div className="list">
+                    <h3>Contactanos</h3>
+                    <a href="#">Correo</a>
+                    <a href="#">Telefono</a>
+                </div>
+            </div>
+            <div className="box">
+                <button className = "button2">Inicio</button>
+                </div>
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer;
