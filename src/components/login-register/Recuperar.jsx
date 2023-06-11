0acea6dae9652522f53a72b96deb435674c5908f
Cris25nkl <cristian25florez@gmail.com>
2023-06-11 12:23:31 -0500

    Update: Recuperar

---
+++ src/components/login-register/Recuperar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import RecuperarDesc from "../RecuperarDescripcion";
+import estilo from "../../Styles/RecuperarP.module.css";
+import Logo from "../Logo";
+
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function Recuperar() {
+  return (
+    <div className={estilo.cont3}>
+      <RecuperarDesc />
+
+      <div className={estilo.cont2}>
+        <Logo />
+        <p className={estilo.text1}>
+          <b>!Recupera tu contraseña¡</b>
+        </p>
+        <p className={estilo.text2}>
+          Introduce tu correo electrónico a continuación para recuperar tu
+          contraseña
+        </p>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Correo"
+          className="mb-3"
+        >
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            id={estilo.inpt1}
+          />
+        </FloatingLabel>
+        <Button variant="warning" id={estilo.btn1}>
+          {" "}
+          Enviar.
+        </Button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default Recuperar;
---
+++ src/components/login-register/Recuperar3.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import RecuperarDesc from "../../components/RecuperarDescripcion";
+import estilo from "../../Styles/Recuperar3P.module.css";
+import Logo from "../../components/Logo";
+
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function Recuperar() {
+  return (
+    <div className={estilo.cont3}>
+      <RecuperarDesc />
+
+      <div className={estilo.cont2}>
+        <Logo />
+        <p className={estilo.text1}>
+          <b>Restablece tu contraseña</b>
+        </p>
+        <p className={estilo.text2}>
+El codigo de verificacion a sido comprovado. Ingrese una nuevo contraseña        </p>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Nueva contraseña"
+          className="mb-3"
+        >
+          <Form.Control
+            type="code"
+            placeholder="name@example.com"
+            id={estilo.inpt1}
+          />
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Confirma nueva contraseña"
+          className="mb-3"
+        >
+          <Form.Control
+            type="code"
+            placeholder="name@example.com"
+            id={estilo.inpt1}
+          />
+        </FloatingLabel>
+        <p className={estilo.text3}>
+         
+        </p>
+        <Button variant="warning" id={estilo.btn1}>
+          {" "}
+          Restablecer contraseña
+        </Button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default Recuperar;
