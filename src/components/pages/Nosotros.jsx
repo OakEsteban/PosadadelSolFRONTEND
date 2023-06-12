ac3f7f87f069e962846a7d3d41ba605b05e7941d
Cris25nkl <cristian25florez@gmail.com>
2023-06-11 20:31:05 -0500

    Update: Trabajadores

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -1,39 +1,20 @@
 import React from "react";
-import CardWorker from "../CardWorker";
-import t1 from "../../Images/trabajador1.jpg";
-import t2 from "../../Images/trabajador2.jpg";
-import t3 from "../../Images/trabajador3.jpeg";
-import '../../Styles/Nosotros3.css'
+import WorkerMod from "../WorkerMod";
+
 
 export const Nosotros = () => {
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center",
         alignItems: "center",
-        height: "90vh",
+        flexDirection: 'column',
+        marginTop: '10%',
+        justifyContent:"center"
       }}
     >
-      <div className="container-101">
-        <CardWorker
-          src={t1}
-          Title="Nuestros Recepcionistas"
-          description="Nuestros empleados estan capacitados con las mejores habilidades par la atencion al cliente, estan capacitados para comprender lo que nuestros recidentes requieran."
-        />
-
-        {/* <CardWorker
-          src={t3}
-          Title="Nuestros Cocineros"
-          description="Nuestros empleados estan capacitados con las mejores habilidades par la atencion al cliente, estan capacitados para comprender lo que nuestros recidentes requieran."
-        />
-
-        <CardWorker
-          src={t2}
-          Title="Los empleados de servicio"
-          description="Nuestros empleados estan capacitados con las mejores habilidades par la atencion al cliente, estan capacitados para comprender lo que nuestros recidentes requieran."
-        /> */}
-      </div>
+      <WorkerMod/> 
+      
     </div>
   );
 };
